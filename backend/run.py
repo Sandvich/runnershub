24cdbd9fc36f90567b930363cc1693f281813bc5
Sanchit Sharma <sanchitsharma1@gmail.com>
2017-07-24 12:17:49 +0100

    Deleted the old backend folder

--- backend/run.py
+++
@@ -1,4 +0,0 @@
-from runnershub import app
-
-if __name__ == '__main__':
-    app.run()
--- backend/runnershub/main/controllers.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, Response
-import json
-
-main = Blueprint('main', __name__)
-
-@main.route('/')
-def index():
-	return Response(json.dumps({'response':'Welcome!'}), status=200, mimetype='application/json')
